79a1cf055db81de0b4c120743822a8f7627acf89
Ponthawan <s6506022610031@email.kmutnb.ac.th>
2023-07-11 08:42:18 +0700

    Update

---
+++ lecture2/index.js
@@ -0,0 +1,10 @@
+const message = "Hello";
+const place = "World";
+
+//logging out a string
+console.log('Hello, World!');
+
+//using substitution
+console.log('Hello, %s!', place);
+
+console.log(`${message}, ${place}!`);
