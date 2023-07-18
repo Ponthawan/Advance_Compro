ae2969d384b562140b2d6ec9212219beda7ab6eb
Ponthawan <s6506022610031@email.kmutnb.ac.th>
2023-07-18 10:10:35 +0700

    Update

---
+++ lecture3/this_keyword2.js
@@ -0,0 +1,19 @@
+var name = "Fatema";
+
+function fun(){
+    // some code here
+    console.log(this.name);
+}
+
+const user = {
+    name: "Marium",
+    yearOfBirth: 1999,
+    calcAge: function(){
+        const currentYear = (new Date()).getFullYear();
+        return currentYear - this.yearOfBirth;
+    }
+}
+
+fun();
+user.calcAge();
+fun.call(user);
